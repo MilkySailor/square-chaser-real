53afc0d436a06a9559a101adb2958ebddce7c537
P4 <cooptebb133@ed.amdsb.ca>
2021-12-06 12:37:33 -0500

    Add project files.

---
+++ square chaser real/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace square_chaser_real
+{
+    partial class squareChaser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.P1speedBoostTimer = new System.Windows.Forms.Timer(this.components);
+            this.P2speedBoostTimer = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 25;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(194, 16);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(27, 19);
+            this.p1ScoreLabel.TabIndex = 0;
+            this.p1ScoreLabel.Text = "XX";
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.AutoSize = true;
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(360, 16);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(27, 19);
+            this.p2ScoreLabel.TabIndex = 1;
+            this.p2ScoreLabel.Text = "XX";
+            // 
+            // P1speedBoostTimer
+            // 
+            this.P1speedBoostTimer.Interval = 1000;
+            this.P1speedBoostTimer.Tick += new System.EventHandler(this.P1speedBoostTimer_Tick);
+            // 
+            // P2speedBoostTimer
+            // 
+            this.P2speedBoostTimer.Interval = 1000;
+            this.P2speedBoostTimer.Tick += new System.EventHandler(this.P2speedBoostTimer_Tick);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(155, 207);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(271, 34);
+            this.winLabel.TabIndex = 2;
+            this.winLabel.Text = "Player X Wins!!!";
+            this.winLabel.Visible = false;
+            // 
+            // squareChaser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "squareChaser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Square Chaser";
+            this.TopMost = true;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.squareChaser_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.squareChaser_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.squareChaser_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Timer P1speedBoostTimer;
+        private System.Windows.Forms.Timer P2speedBoostTimer;
+        private System.Windows.Forms.Label winLabel;
+    }
+}
+
---
+++ square chaser real/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace square_chaser_real
+{
+    public partial class squareChaser : Form
+    {
+        Random TeleporterXRan = new Random();
+        Random pointYRan = new Random();
+        Random pointXRan = new Random();
+        Random speedBoostXRan = new Random();
+        Random speedBoostYRan = new Random();
+
+        Rectangle player1 = new Rectangle(120, 210, 20, 20);
+        Rectangle player2 = new Rectangle(240, 120, 20, 20);
+        Rectangle point = new Rectangle(295, 195, 10, 10);
+        Rectangle speedBoost = new Rectangle(130, 170, 10, 10);
+        Rectangle border = new Rectangle(4, 5,575,552);
+        Rectangle teleporter = new Rectangle(380, 280, 15, 15);
+        
+        int player1Score = 0;
+        int player2Score = 0;
+        int P1timerticks = 0;
+        int P2timerticks = 0;   
+
+        int player1Speed = 6;
+        int player2Speed = 6;
+        int speedBoostTime = 0;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftArrowDown = false;
+        bool rightArrowDown = false;
+
+        SolidBrush redBrush = new SolidBrush(Color.DarkRed);
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush greenBrush = new SolidBrush(Color.ForestGreen);
+        Pen Pen = new Pen(Color.CornflowerBlue, 10);
+        Pen tele = new Pen(Color.Purple, 3);
+        public squareChaser()
+        {
+            InitializeComponent();
+        }
+
+        private void squareChaser_KeyDown(object sender, KeyEventArgs e)
+        {
+            //movement
+            switch (e.KeyCode)
+            {
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+
+        private void squareChaser_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void squareChaser_Paint(object sender, PaintEventArgs e)
+        {
+
+            e.Graphics.DrawRectangle(Pen, border);
+            e.Graphics.FillEllipse(blueBrush, player1);
+            e.Graphics.FillEllipse(redBrush, player2);
+            e.Graphics.FillRectangle(greenBrush, point);
+            e.Graphics.FillEllipse(yellowBrush, speedBoost);
+            e.Graphics.DrawRectangle(tele, teleporter);
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //p1 
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += player1Speed;
+            }
+
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= player1Speed;
+            }
+            if (dDown == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X += player1Speed;
+            }
+            //p2
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= player2Speed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += player2Speed;
+            }
+            if (leftArrowDown == true && player2.X > 0)
+            {
+                player2.X -= player2Speed;
+            }
+            if (rightArrowDown == true && player2.X < this.Width - player2.Width)
+            {
+                player2.X += player2Speed;
+            }
+
+
+            //check if either player has interacted with anything
+            if (player1.IntersectsWith(point))
+            {
+                player1Score++;
+                pointXRan.Next(1, 580);
+                pointYRan.Next(1, 580);
+                point.X = pointXRan.Next(1, 530);
+                point.Y = pointXRan.Next(1, 530);
+
+            }
+            if (player2.IntersectsWith(point))
+            {
+                player2Score++;
+                point.X = pointXRan.Next(1, 530);
+                point.Y = pointXRan.Next(1, 530);
+            }
+            if(player1.IntersectsWith(speedBoost))
+            {
+                speedBoost.Y = speedBoostYRan.Next(1, 580);
+                speedBoost.X = speedBoostXRan.Next(1, 580);
+                player1Speed = 12;
+
+                P1speedBoostTimer.Enabled = true;
+            }
+            if (player2.IntersectsWith(speedBoost))
+            {
+                speedBoost.Y = speedBoostYRan.Next(1, 580);
+                speedBoost.X = speedBoostXRan.Next(1, 580);
+                player2Speed = 12;
+                
+                P2speedBoostTimer.Enabled =true;
+            }
+            if(player1.IntersectsWith(teleporter))
+            {
+                teleporter.X = TeleporterXRan.Next(30, 530);
+                teleporter.Y = TeleporterXRan.Next(30, 530);
+                
+                player1.Y = TeleporterXRan.Next(30, 530);
+                player1.X = TeleporterXRan.Next(30, 530);
+            }
+            if (player2.IntersectsWith(teleporter))
+            {
+                teleporter.X = TeleporterXRan.Next(30, 530);
+                teleporter.Y = TeleporterXRan.Next(30, 530);
+                
+                player2.Y = TeleporterXRan.Next(30, 530);
+                player2.X = TeleporterXRan.Next(30, 530);
+            }
+
+            //score
+            p1ScoreLabel.Text = $"{player1Score}";
+            p2ScoreLabel.Text = $"{player2Score}";
+            if(player1Score == 7)
+            {
+                winLabel.Visible = true;
+                winLabel.Text = "Player 1 Wins!!!";
+                gameTimer.Enabled = false;
+            }
+            if (player2Score == 7)
+            {
+                winLabel.Visible = true;
+                winLabel.Text = "Player 2 Wins!!!";
+                gameTimer.Enabled = false;
+            }
+
+            // wall teleports player 1
+            if (player1.X < 9)
+            {
+                player1.X = this.Width - 30;
+            }
+            if(player1.X > this.Width - 30)
+            {
+                player1.X = 9;
+            }
+            if(player1.Y < 9)
+            {
+                player1.Y = this.Height - 30;
+            }
+            if (player1.Y > this.Height - 30)
+            {
+                player1.Y = 9;
+            }
+
+            //wall teleports player 2
+            if (player2.X < 9)
+            {
+                player2.X = this.Width - 30;
+            }
+            if (player2.X > this.Width - 30)
+            {
+                player2.X = 9;
+            }
+            if (player2.Y < 9)
+            {
+                player2.Y = this.Height - 30;
+            }
+            if (player2.Y > this.Height - 30)
+            {
+                player2.Y = 9;
+            }
+
+            Refresh();
+        }
+
+        private void P1speedBoostTimer_Tick(object sender, EventArgs e)
+        {
+            P1timerticks++;
+
+            if(P1timerticks > 5)
+            {
+                player1Speed = 6;
+                P1timerticks = 0;
+                P1speedBoostTimer.Enabled = false;
+            }
+        }
+
+        private void P2speedBoostTimer_Tick(object sender, EventArgs e)
+        {
+            P2timerticks++;
+
+            if(P2timerticks > 5)
+            {
+                player2Speed = 6;
+                P2timerticks = 0;
+                P2speedBoostTimer.Enabled = false;
+            }
+        }
+    }
+}
